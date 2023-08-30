8766370e60d1c3fec3c61307a33a962ab7bd37b7
juliedosher <juliegdosher@lewisu.edu>
2023-08-30 12:55:35 -0500

    fixed dark/light mode and header

--- script/main.js
+++ script/main.js
@@ -3,13 +3,13 @@ var isDark = true;              // default value - dark mode on
 function toggleLightMode() {
     if (isDark) {
         document.getElementById('style-mode').setAttribute('href', './style/light.css');
-        document.getElementById("nav").className = "navbar navbar-expand-sm bg-light navbar-light";
+        document.getElementById("nav").className = "navbar navbar-expand-sm bg-light navbar-light navbar-fixed-top";
         document.getElementById("modeImage").src = './img/moon.png';
         isDark = false;
    
     } else {
         document.getElementById('style-mode').setAttribute('href', './style/dark.css');
-        document.getElementById("nav").className = "navbar navbar-expand-sm bg-dark navbar-dark";
+        document.getElementById("nav").className = "navbar navbar-expand-sm bg-dark navbar-dark navbar-fixed-top";
         document.getElementById("modeImage").src = './img/sun.png';
         isDark = true;
     }
